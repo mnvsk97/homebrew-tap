26fbb2509d44289c03b63382f4c38607ed9fa759
M N V Sai Krishna <mnvsk97@gmail.com>
2026-04-15 15:45:27 -0400

    harnessctl 0.1.0

--- Formula/harnessctl.rb
+++ Formula/harnessctl.rb
@@ -7,22 +7,22 @@ class Harnessctl < Formula
   on_macos do
     on_arm do
       url "https://github.com/mnvsk97/harnessctl/releases/download/v#{version}/harnessctl-darwin-arm64"
-      sha256 "PLACEHOLDER"
+      sha256 "edc330438af87c22b500e49a008c6d218233b4b28ee5f4903b468890e7246a0e"
     end
     on_intel do
       url "https://github.com/mnvsk97/harnessctl/releases/download/v#{version}/harnessctl-darwin-x64"
-      sha256 "PLACEHOLDER"
+      sha256 "761b1bc55b49854ad95391442e309f82f2bac967054d26ada63b8b8274fa256a"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/mnvsk97/harnessctl/releases/download/v#{version}/harnessctl-linux-arm64"
-      sha256 "PLACEHOLDER"
+      sha256 "a6203a5df7b3817c16690449bca09b7965ee8ce42db701fb51693b330a97d5a1"
     end
     on_intel do
       url "https://github.com/mnvsk97/harnessctl/releases/download/v#{version}/harnessctl-linux-x64"
-      sha256 "PLACEHOLDER"
+      sha256 "4951c2c55cbcb11a41fba265d557aa470e3b059c1bbd870851e812845374772b"
     end
   end
 
